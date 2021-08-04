09f9b5bf940f6411f9e943222d7691c60e6b17b1
Justin Field <fieldju@gmail.com>
2021-08-04 12:13:15 -0700

    feat: port potato-facts into CLI as a potato-fact command

--- cmd/root.go
+++ cmd/root.go
@@ -13,7 +13,7 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "awesom-o",
-	Short: "#dev-life",
+	Short: "Greetings. I am the AWESOM-O 4000.",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
